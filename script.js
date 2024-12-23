e2f42ba1ee74cf80914985041927c4c75ba2b36c
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-23 12:49:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let color=document.getElementById("colorSelect");
+    function SR() {
+        let selectedOption=color.selectedIndex+1;
+        color.removeChild(document.getElementById(selectedOption));
+    }
